68a3232de90f818c0805d72fba5d06b2f4390aa2
Fariman <farimanghaedi@gmail.com>
2023-09-25 17:50:54 +0330

    [Modified]: Refactoring the code: adding to helper methods _check_events and _update_screen.

--- space_ship.py
+++ space_ship.py
@@ -20,16 +20,21 @@ class SpaceShip:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            self.screen.fill(self.background_color)
-            self.ship.blit_me()
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        # Respond to keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.background_color)
+        self.ship.blit_me()
+        pygame.display.flip()
 
 
 if __name__ == '__main__':

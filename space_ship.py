77bdfa1af0a6d648d69e48dd1b410417e8088361
Fariman <farimanghaedi@gmail.com>
2023-09-29 16:38:46 +0330

    [Modified]: Deleting disappeared bullets

--- space_ship.py
+++ space_ship.py
@@ -26,6 +26,10 @@ class SpaceShip:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+            # Get rid of bullets that have disappeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
             self._update_screen()
             self.clock.tick(60)
 
